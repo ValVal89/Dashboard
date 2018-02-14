c2dfe757d654146234436b8222af2e36d773dfe8
vmartinovich <mv26091989@gmail.com>
2018-02-14 17:49:21 +0300

    add JSONObject to convert HashMap to Json

--- src/main/java/com/mongoDB/vacation/sample/Utils.java
+++ src/main/java/com/mongoDB/vacation/sample/Utils.java
@@ -2,11 +2,9 @@ package com.mongoDB.vacation.sample;
 
 import com.mongoDB.vacation.sample.model.LayoutSettings;
 import com.mongoDB.vacation.sample.model.User;
+import org.json.JSONObject;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Utils
 {
@@ -14,7 +12,7 @@ public class Utils
     private static Map<String, Object> settings2 = new HashMap<>();
     private static LayoutSettings layoutSettings = new LayoutSettings(1, "name", settings);
     private static LayoutSettings layoutSettings2 = new LayoutSettings(2, "name", settings2);
-    private static List<LayoutSettings> list= new ArrayList<>();
+    private static Set<LayoutSettings> list= new HashSet<>();
 
     public static void main(String[] args) {
 
@@ -22,6 +20,12 @@ public class Utils
 
         User createdUser = new User("id", "firstName", "lastName", "email", "pass", list);
             System.out.println(createdUser);
+
+        JSONObject json = new JSONObject(settings);
+        System.out.println(json);
+
+        JSONObject json2 = new JSONObject(settings2);
+        System.out.println(json2);
         }
 
         public static void fill()
